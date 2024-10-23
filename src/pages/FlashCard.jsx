893fc526c58b9b6a2694fbcc82f897a2000eb80d
Loren Lei <22lorenlei@gmail.com>
2024-10-22 23:44:08 -0400

    WIP making it so you can switch from level to level

--- src/pages/FlashCard.jsx
+++ src/pages/FlashCard.jsx
@@ -1,28 +1,42 @@
 import React, { useState, useEffect } from 'react';
 
-function getHSK1Data() {
-  const [hsk1Set, setHsk1Set] = useState(null);
+let hsk1JsonURL = "https://raw.githubusercontent.com/clem109/hsk-vocabulary/refs/heads/master/hsk-vocab-json/hsk-level-2.json";
 
+let hsk2JsonURL = "https://raw.githubusercontent.com/clem109/hsk-vocabulary/refs/heads/master/hsk-vocab-json/hsk-level-2.json";
+
+let hsk3JsonURL = "https://raw.githubusercontent.com/clem109/hsk-vocabulary/refs/heads/master/hsk-vocab-json/hsk-level-3.json";
+
+let hsk4JsonURL = "https://raw.githubusercontent.com/clem109/hsk-vocabulary/refs/heads/master/hsk-vocab-json/hsk-level-4.json";
+
+let hsk5JsonURL = "https://raw.githubusercontent.com/clem109/hsk-vocabulary/refs/heads/master/hsk-vocab-json/hsk-level-5.json";
+
+let hsk6JsonURL = "https://raw.githubusercontent.com/clem109/hsk-vocabulary/refs/heads/master/hsk-vocab-json/hsk-level-6.json";
+
+function getHSK1Data(chosenURL) {
+  let [hskSet, setHskSet] = useState(null);
   useEffect(() => {
-    fetch('https://raw.githubusercontent.com/clem109/hsk-vocabulary/refs/heads/master/hsk-vocab-json/hsk-level-1.json')
+    fetch(chosenURL)
       .then((response) => response.json())
       .then((responseJson) => {
-        setHsk1Set(responseJson);
+        setHskSet(responseJson);
       })
       .catch((error) => {
         console.log(error);
       });
   }, []);
-
-  return hsk1Set;
+  console.log(hskSet);
+  return hskSet;
 }
 
 function flashCardSet1() {
-  const hsk1Set = getHSK1Data();
 
   const [isVisible, setIsVisible] = useState(false);
 
   const [count, setCount] = useState(0);
+
+  const [hskURL, setHskURL] = useState(hsk1JsonURL);
+
+  let hskSet = getHSK1Data(hskURL);
 
   const increment = () => {
     setCount(prevCount => prevCount + 1);
@@ -32,11 +46,41 @@ function flashCardSet1() {
     setCount(prevCount => prevCount - 1);
   }
 
+  const changeHSK1 = () => {
+    setCount(0);
+    setHskURL(hsk1JsonURL)
+  }
+
+  const changeHSK2 = () => {
+    setCount(0);
+    setHskURL(hsk2JsonURL);
+  }
+
+  const changeHSK3 = () => {
+    setCount(0);
+    setHskURL(hsk3JsonURL);
+  }
+
+  const changeHSK4 = () => {
+    setCount(0);
+    setHskURL(hsk4JsonURL);
+  }
+
+  const changeHSK5 = () => {
+    setCount(0);
+    setHskURL(hsk5JsonURL);
+  }
+
+  const changeHSK6 = () => {
+    setCount(0);
+    setHskURL(hsk6JsonURL);
+  }
+
   const toggleVisibility = () => {
     setIsVisible(!isVisible);
   }
   
-  if (!hsk1Set) {
+  if (!hskSet) {
     return <p>Loading...</p>;
   }
   
@@ -51,14 +95,32 @@ function flashCardSet1() {
       <button onClick={decrement}> 
           Go back
       </button>
+      <button onClick={changeHSK1}>
+        HSK1
+      </button>
+      <button onClick={changeHSK2}>
+        HSK2
+      </button>
+      <button onClick={changeHSK3}>
+        HSK3
+      </button>
+      <button onClick={changeHSK4}>
+        HSK4
+      </button>
+      <button onClick={changeHSK5}>
+        HSK5
+      </button>
+      <button onClick={changeHSK6}>
+        HSK6
+      </button>
       {isVisible && (
         <div>
-          { hsk1Set[count].hanzi }
+          { hskSet[count].hanzi }
         </div>
       )}
       {!isVisible && (
         <div>
-          { hsk1Set[count].pinyin }
+          { hskSet[count].pinyin }
         </div>
       )}
     </>
